2d08ebb67693c4494ee8f98fd934db67e657313b
Sam LaChapelle <sambosauce@gmail.com>
2021-06-20 22:44:43 -0400

    added more commands and edited variable/room names, descriptions.

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ class Rooms {
   }
   take(item) {
     if (roomLookUp[currentRoom].inventory[item] === undefined) {
-      return "This item doesn't exist...\n>";
+      return "This item does not exist...\n>";
     } else if (roomLookUp[currentRoom].inventory[item].takeable === false) {
-      return "This item is already in your inventory...\n>";
+      return "This item is already in your inventory or it cannot be taken...\n>";
     } else {
       playerInv[item] = roomLookUp[currentRoom].inventory[item];
       roomLookUp[currentRoom].inventory[item].takeable = false;
@@ -38,236 +38,188 @@ class Rooms {
   drop(item) {
     if (playerInv[item] != undefined) {
       delete playerInv[item];
+      roomLookUp[currentRoom].inventory[item].takeable = true;
       return "Successfully dropped " + item + "...";
     }
     return "Item " + item + " is not in your inventory";
   }
-}
-
-function getPlayerInv() {
-  let responseString = "Current Inventory:\n";
-  for (let item in playerInv) {
-    responseString += playerInv[item].name + ",\n";
+  use(item) {
+    if (roomLookUp[currentRoom].inventory[item].name === "first key") {
+      delete playerInv[item];
+      return "You have unlocked the door to The Billiard room...\n>";
+    } else if (roomLookUp[currentRoom].inventory[item].name === "second key") {
+      delete playerInv[item];
+      return "You have unlocked the door to The Library...\n>";
+    } else if (roomLookUp[currentRoom].inventory[item].name === "third key") {
+      delete playerInv[item];
+      return "You have unlocked the door to the Study...\n>";
+    } else if (roomLookUp[currentRoom].inventory[item].name === "computer") {
+      return roomLookUp[currentRoom].inventory[item].desc;
+    } else if (roomLookUp[currentRoom].inventory[item].name === "keypad") {
+      return roomLookUp[currentRoom].inventory[item].desc;
+    } else if (roomLookUp[currentRoom].inventory[item].name === "carKey") {
+      return "As Post Malone said..'Congratulations!!!'. You've beaten my Escape The Room!!";
+    }
   }
-  return responseString;
+  moveTo(roomName) {
+    playerLoc(roomName);
+  }
+  read(item) {
+    return roomLookUp[currentRoom].inventory[item].desc;
+  }
+  type(password) {
+    if (password === "garrison") {
+      return "You hear a door unlock from the other room...maybe it leads to the Kitchen...\n>";
+    } else {
+      return "Password Invalid...please try again...\n>";
+    }
+  }
+  enter(code) {
+    if (code === "5183") {
+      return "The keypad glows green and unlocks the Shed...\n>";
+    } else {
+      return "The keypad buzzes. Try another code...\n>";
+    }
+  }
 }
 
 /* Seven Room Instances */
 
-let roomOne = new Rooms(
+let Lounge = new Rooms(
   "Lounge",
   {
-    firstKey: {
+    key: {
       name: "first key",
       desc: "You have found your first key!\n>",
-      takeable: true,
-    },
-    candleStick: {
-      name: "candle stick",
-      desc: "I wonder if there is something we can light it with...\n>",
-      takeable: true,
-    },
-    matchBox: {
-      name: "match box",
-      desc: "Maybe we can use it to light something...\n>",
-      takeable: true,
-    },
-    bottle: {
-      name: "bottle",
-      desc: "As you picked it up you heard a *clink* from inside the bottle...\n>",
       takeable: true,
     },
   },
   "A dim lamp fills the room allowing you to see a few scattered chairs, a round table in the center of the room and a door across the room. On this table you see a dark brown colored whiskey bottle next to a cigarette put out in an ash tray. Laying on one of the chairs is a candle stick and a match box. What are you going to do next?\n>"
 );
 
-let roomTwo = new Rooms(
+let Billiard = new Rooms(
   "Billiard Room",
-  [
-    (secondKey = {
+  {
+    key: {
       name: "second key",
       desc: "You have found your second key!\n>",
       takeable: true,
-    }),
-    (eightBall = {
-      name: "8 ball",
-      desc: "As you feel the weight of the pool ball in your hand you have a flashback of a time (you were playing billiards with all of your lads in your favorite pub) and smirk",
+    },
+    painting: {
+      name: "painting",
+      desc: "As you take the painting off the wall, a key drops on to the floor from behind the painting...\n>",
       takeable: true,
-    }),
-    (nightStand = {
-      name: "night stand",
-      desc: "Above the two locked drawers you a layer of dust covering the top of the night stand",
-      takeable: false,
-    }),
-  ],
-  "desc"
+    },
+  },
+  "Walking in to The Billiard Room, you see a pool table a few chairs surrounding and an odd painting hanging above one of the chairs...\n>"
 );
 
-let roomThree = new Rooms(
+let Library = new Rooms(
   "Library",
-  [
-    (bookCase = {
-      name: "book case",
-      desc: "From the floor to the ceiling the book case towers over you. You notice a few books are missing",
-      takeable: false,
-    }),
-    (bookOne = {
-      name: "book one",
-      desc: "The front cover reads 'Peaky Blinders' in a bold old english font', and on the first page it reads 'Arthur Shelby 1940' in the bottom left hand corner...\n>",
+  {
+    book: {
+      name: "book",
+      desc: "As you open the book, it seems like a normal but you flipped a few pages revealing a third key laying in the hidden cut out of the book!\n>",
       takeable: true,
-    }),
-    (bookTwo = {
-      name: "book two",
-      desc: "The front cover reads 'Peaky Blinders ' in a bold old english font', and on the first page it reads 'Thomas Shelby 1942' in the bottom left hand corner...\n>",
-      takeable: true,
-    }),
-    (bookThree = {
-      name: "book three",
-      desc: "The front cover reads 'Peaky Blinders' in a bold old english font', and on the first page it reads 'John Shelby 1945' in the bottom left hand corner...\n>",
-      takeable: true,
-    }),
-    (secretCompartment = {
-      name: "secret compartment",
-      desc: "Something seems off about this part of the wall but you can't seem to put your finger on it\n>",
-      takeable: false,
-    }),
-    (thirdKey = {
+    },
+    key: {
       name: "third key",
       desc: "You have found your third key!\n>",
       takeable: true,
-    }),
-  ],
-  "desc"
+    },
+  },
+  "Walking in to The Library, you see book shelves wrapping around the room and a table in the very center of the room. Sitting on the table is one book titled 'The Shelby's'...\n>"
 );
 
-let roomFour = new Rooms(
+let Study = new Rooms(
   "Study",
-  [
-    (computer = {
+  {
+    computer: {
       name: "computer",
       desc: "The computer screen radiates a soft light blue across the room and reads 'Password...<_>'...\n>",
       takeable: false,
-    }),
-    (deskDrawer = {
-      name: "desk drawer",
-      desc: "You open the drawer and see the keyboard and a note pad...\n>",
-      takeable: false,
-    }),
-    (keyboard = {
-      name: "keyboard",
-      desc: "You blow the dust from off the key board clouding the air in front of you *coughs*...\n>",
-      takeable: false,
-    }),
-    (notePad = {
-      name: "note pad",
-      desc: "a scrambled word on the page reads 'rsonrgia'...\n>",
-      takeable: false,
-    }),
-  ],
-  "desc"
+    },
+  },
+  "Walking in to The Study, there is only a computer on a desk and a note pad that has a scrambled word written on it that says 'rsonrgia'...\n>"
 );
 
-let roomFive = new Rooms(
+let Kitchen = new Rooms(
   "Kitchen",
-  [
-    (fridge = {
-      name: "fridge",
-      desc: "You notice scattered on the front door there are four magnets with a number each '8', '3', '5', and '1'. Opening the fridge there is bread and a jug holding an unknown liquid...\n>",
-      takeable: false,
-    }),
-    (bread = {
-      name: "bread",
-      desc: "*spits out bread* EW, that has mold on it. Quick you need something to wash the mold out of your mouth or you will catch a disease!\n>",
-      takeable: true,
-    }),
-    (jug = {
-      name: "jug",
-      desc: "*spits out milk* UGH, why would drink an unknown liquid you fool! Who knows how old that milk could be?...\n>",
-      takeable: true,
-    }),
-    (pan = {
-      name: "pan",
-      desc: "Hm a cast iron pan. That could be useful...\n>",
-      takeable: true,
-    }),
-  ],
-  "desc"
+  {},
+  "As you push the door open, the first thing you notice is four magnets on the fridge door that read '8', '3', '5', and '1'. next to the fridge there is an open door that leads to the backyard...\n>"
 );
 
-let roomSix = new Rooms(
+let Backyard = new Rooms(
   "Backyard",
-  [
-    (keypad = {
+  {
+    keypad: {
       name: "keypad",
       desc: "Enter code...\n>",
       takeable: false,
-    }),
-    (car = {
+    },
+    car: {
       name: "car",
       desc: "The car door is locked, you need the cars keys...\n>",
       takeable: false,
-    }),
-  ],
-  "desc"
+    },
+  },
+  "Feeling the cold air on your skin as you step outside into the Backyard, you see a car and a shed with a keypad on it...\n>"
 );
 
-let roomSeven = new Rooms(
+let Shed = new Rooms(
   "Shed",
-  [
-    (lightSwitch = {
-      name: "light switch",
-      desc: "covered in cobwebs, you flicked the light switch on for the light to reveal 3 keys (blue key, red key and yellow key)...\n>",
-      takeable: false,
-    }),
-    (blueKey = { name: "blue key", desc: "hmm...\n>", takeable: true }),
-    (redKey = {
-      name: "red key",
-      desc: "Interesting...\n>",
+  {
+    carKey: {
+      name: "car key",
+      desc: "car key has been added to your inventory...\n>",
       takeable: true,
-    }),
-    (yellowKey = {
-      name: "yellow key",
-      desc: "Alright then, if that's what you want...\n>",
-      takeable: true,
-    }),
-  ],
-  "desc"
+    },
+  },
+  "As the moonlight creeps through the shed windows you see your ticket home hanging in front of you...a singular car key...\n>"
 );
 
 /* Room Look Up Table */
 
 let roomLookUp = {
-  roomOne: roomOne,
-  roomTwo: roomTwo,
-  roomThree: roomThree,
-  roomFour: roomFour,
-  roomFive: roomFive,
-  roomSix: roomSix,
-  roomSeven: roomSeven,
+  Lounge: Lounge,
+  Billiard: Billiard,
+  Library: Library,
+  Study: Study,
+  Kitchen: Kitchen,
+  Backyard: Backyard,
+  Shed: Shed,
 };
 
 /* Player Inventory */
+
+function getPlayerInv() {
+  let responseString = "Current Inventory:\n";
+  for (let item in playerInv) {
+    responseString += playerInv[item].name + "\n";
+  }
+  return responseString;
+}
 
 let playerInv = {};
 
 /* State/Move Machine */
 
-let currentRoom = "roomOne";
+let currentRoom = "Lounge";
 
 let rooms = {
-  roomOne: ["roomTwo"],
-  roomTwo: ["roomOne", "roomThree", "roomFour", "roomFive"],
-  roomThree: ["roomTwo"],
-  roomFour: ["roomTwo"],
-  roomFive: ["roomTwo", "roomSix"],
-  roomSix: ["roomFive", "roomSeven"],
-  roomSeven: ["roomSix"],
+  Lounge: ["Billiard"],
+  Billiard: ["Lounge", "Library", "Study", "Kitchen"],
+  Library: ["Billiard"],
+  Study: ["Billiard"],
+  Kitchen: ["Billiard", "Backyard"],
+  Backyard: ["Kitchen", "Shed"],
+  Shed: ["Backyard"],
 };
 
 function playerLoc(nextLoc) {
   if (rooms[currentRoom].includes(nextLoc)) {
     currentRoom = nextLoc;
-    return roomLookUp[currentRoom].describe();
+    console.log(roomLookUp[currentRoom].describe());
   } else {
     return "You cannot currently access this location...\n>";
   }
@@ -279,7 +231,7 @@ playerLoc();
 textAdventure();
 
 async function textAdventure() {
-  const welcomeMessage = `Greetings Stranger! You will be playing an Escape the Room Text Adventure: 1940's (War Year's Themed). You will be able to use commands like [take, drop, use/throw, open, read, eat, drink etc.. ], You can also type "inv" to check your inventory.\nWhen you are ready type "begin" to be captured and locked inside "The Garrison" as a 1940's gangster who fixes horse races and gambles their life against all odds to become a success in the harsh society of Birmingham, England...\n>`;
+  const welcomeMessage = `Greetings Stranger! You will be playing an Escape the Room Text Adventure: 1940's (War Year's Themed). You will be able to use commands like [take, drop, use, read, type, enter, and move to (move to 'room name ex. Billiard')], You can also type "Inv" to check your inventory.\nWhen you are ready type "begin" to be captured and locked inside "The Garrison" as a 1940's gangster who fixes horse races and gambles their life against all odds to become a success in the harsh society of Birmingham, England...\n>`;
 
   let answer = await ask(welcomeMessage);
 
@@ -300,6 +252,18 @@ async function textAdventure() {
       playerComm = await ask(roomLookUp[currentRoom].take(response.slice(5)));
     } else if (response.slice(0, 3).toLowerCase().includes("inv")) {
       playerComm = await ask(getPlayerInv() + "\n>");
+    } else if (response.slice(0, 4).toLowerCase() === "drop") {
+      playerComm = await ask(roomLookUp[currentRoom].drop(response.slice(5)));
+    } else if (response.slice(0, 3).toLowerCase() === "use") {
+      playerComm = await ask(roomLookUp[currentRoom].use(response.slice(4)));
+    } else if (response.slice(0, 4).toLowerCase() === "read") {
+      playerComm = await ask(roomLookUp[currentRoom].read(response.slice(5)));
+    } else if (response.slice(0, 7).toLowerCase() === "move to") {
+      playerComm = await ask(roomLookUp[currentRoom].moveTo(response.slice(8)));
+    } else if (response.slice(0, 4).toLowerCase() === "type") {
+      playerComm = await ask(roomLookUp[currentRoom].type(response.slice(5)));
+    } else if (response.slice(0, 5).toLowerCase() === "enter") {
+      playerComm = await ask(roomLookUp[currentRoom].enter(response.slice(6)));
     } else {
       playerComm = await ask(
         "I didn't understand what you said. Can you repeat that...\n>"
